a56488d6f6895ed6f31baa28e5742699578bdb84
Lucas <lucas.david.car99@gmail.com>
2022-04-28 20:18:21 -0300

    utilidades y funciones varias

---
+++ ejercicios-progra-web-2/utils/elementos-html.php
@@ -0,0 +1,13 @@
+<?php
+
+function header_mio() {
+    echo <<<HEADER
+<h1>Programación Web II</h1>
+HEADER;
+}
+
+function footer_mio(){
+    echo <<<FOOTER
+<p>Lucas David Cardozo - DNI: 41.777.347 - Universidad Nacional de La Matanza</p>
+FOOTER;
+}
