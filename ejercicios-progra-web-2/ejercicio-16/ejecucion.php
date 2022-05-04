8b30347cab9f5d486bcbbc8405a7b19bb6402fc5
Lucas <lucas.david.car99@gmail.com>
2022-05-03 23:57:15 -0300

    algo

--- ejercicios-progra-web-2/ejercicio-16/ejecucion.php
+++ ejercicios-progra-web-2/ejercicio-16/ejecucion.php
@@ -3,9 +3,10 @@
 include_once "./clases/Papel.php";
 include_once "./clases/Piedra.php";
 include_once "./clases/Tijera.php";
+include_once "./clases/Empate.php";
 
-$jugador1 = new Papel("Lucas");
-$jugador2 = new Tijera("Agustina");
+$jugador2 = new Papel("Lucas");
+$jugador1 = new Piedra("Agustina");
 
 $resultado = $jugador1->competirContra($jugador2);
 
